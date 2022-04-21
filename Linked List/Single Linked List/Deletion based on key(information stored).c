8bef402fdd4cb43fdf4f51060ac073dfc087621e
Yuvaraj Rayamane <102849877+YuvarajRayamane@users.noreply.github.com>
2022-04-21 11:50:39 +0530

    Update Deletion based on key(information stored).c

--- Linked List/Single Linked List/Deletion based on key(information stored).c
+++ Linked List/Single Linked List/Deletion based on key(information stored).c
@@ -1,1 +1,34 @@
+C coad:
+struct node{
+  int data;
+  struct node *addr;
+};
+typedef struct node *NODE;
+NODE start;
+
+NODE deletkey(NODE start, int key)
+{
+  NODE prev, cur, temp;
+  if(key == start->data){
+    temp = start;
+    start = start->addr;
+    printf("Node deleted is %d",temp->data);
+    free(temp);
+    return start;
+  }
+  cur = start;
+  prev = NULL;
+  while(cur!=NULL && key!=cur->data){
+    prev = cur;
+    cur = cur->addr;
+  }
+  if(cur == NULL){
+    printf("Key not found!!\n");
+    return start;
+  }
+  prev->addr = cur->addr;
+  printf("Node deleted is %d ",cur->data);
+  free(cur);
+  return start;
+}
 
