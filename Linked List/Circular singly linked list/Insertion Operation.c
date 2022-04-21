1e58ddc83a4a3ba56399876700bb140461c5266d
Yuvaraj Rayamane <102849877+YuvarajRayamane@users.noreply.github.com>
2022-04-21 12:30:10 +0530

    Update Insertion Operation.c

--- Linked List/Circular singly linked list/Insertion Operation.c
+++ Linked List/Circular singly linked list/Insertion Operation.c
@@ -1,1 +1,38 @@
+C coad:
+struct node{
+  int data;
+  struct node *addr;
+};
+typedef struct node *NODE;
+NODE last;
+
+//Insertion at begining
+NODE insertbegin(NODE last, int item)
+{
+  NODE temp;
+  temp = (NODE)malloc(sizeof(struct node));
+  temp->data = item;
+  if(last==NULL){
+    temp->addr = temp;
+    return temp;
+  }
+  temp->addr=last->addr;
+  last->addr=temp;
+  return last;
+}
+
+//Insertion at end
+NODE insertend(NODE last,int item)
+{
+ NODE temp;
+  temp = (NODE)malloc(sizeof(struct node));
+  temp->data = item;
+  if(last==NULL){
+    temp->addr = temp;
+    return temp;
+  }
+  temp->addr=last->addr;
+  last->addr=temp;
+  return temp;
+}
 
